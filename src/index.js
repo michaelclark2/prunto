11e6cfbdb8d516d5066034dcd51dc506423c8bb7
Michael Clark <michael.clark1992@gmail.com>
2022-05-01 16:38:35 -0500

    added bootstrap, and css to webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+const main = document.querySelector("#root");
+
+main.innerHTML = "<h1>Prunto</h1>";
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = [
-  {
-    entry: "./src/index.js",
-    output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "dist"),
-    },
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
-];
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Prunto",
+      template: "src/index.html",
+    }),
+  ],
+};
