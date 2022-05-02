d809c09d8ec9fd92eef77d01959e1aac8c146dc1
Michael Clark <michael.clark1992@gmail.com>
2022-05-02 05:14:03 -0500

    added sourcemap to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
