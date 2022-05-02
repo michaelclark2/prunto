919f1ba27b6d185bf890bd288de25aeaad2d79c8
Michael Clark <michael.clark1992@gmail.com>
2022-05-02 05:13:38 -0500

    added click events

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,10 @@ const writeToDom = (id, htmlString) => {
   document.querySelector(id).innerHTML = htmlString;
 };
 
-export default { writeToDom };
+const clearActiveNavlinks = () => {
+  document
+    .querySelectorAll(".nav-link.active")
+    .forEach((navlink) => navlink.classList.remove("active"));
+};
+
+export default { writeToDom, clearActiveNavlinks };
