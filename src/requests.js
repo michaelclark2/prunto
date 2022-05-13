ab9c9d9f48821d5d9b5fb0f11aea857273f10f41
Michael Clark <michael.clark1992@gmail.com>
2022-05-13 11:49:16 -0500

    gets requests and displays!  todo: flesh out cards, add send request

--- src/requests.js
+++ src/requests.js
@@ -4,10 +4,8 @@ import wallet from "./wallet";
 let requests = [];
 
 const getLoanRequests = async () => {
-  const _loanRequestsLength = await wallet
-    .getContract()
-    .methods.getRequestsLength()
-    .call();
+  const contract = wallet.getContract();
+  const _loanRequestsLength = await contract.methods.getRequestsLength().call();
   const _loanRequests = [];
   for (let i = 0; i < _loanRequestsLength; i++) {
     let _loanRequest = new Promise(async (resolve, reject) => {
@@ -35,7 +33,7 @@ const printRequests = () => {
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Request</h5>
-            <p class="card-text">text</p>
+            <p class="card-text">${req.memo}</p>
         </div>
     </div>
     `
