ee5451e0ba171f4d363bc77eab61ec02aef22fb1
Michael Clark <michael.clark1992@gmail.com>
2022-05-13 10:06:51 -0500

    added loans, accept and deny requests

--- contracts/prunto.sol
+++ contracts/prunto.sol
@@ -9,15 +9,29 @@ contract Prunto {
         address payable requester;
         uint256 amount;
         string memo;
+        bool accepted;
+        bool denied;
     }
+
+    struct Loan {
+        address payable issuer;
+        uint256 amount;
+        // interest_rate
+        // term_length
+        // start_timestamp
+    }
+
     mapping(address => Request[]) internal requests;
+    mapping(address => Loan) internal loans;
 
     function sendRequest(
         address _target,
         uint256 _amount,
         string memory _memo
     ) public {
-        requests[_target].push(Request(payable(msg.sender), _amount, _memo));
+        requests[_target].push(
+            Request(payable(msg.sender), _amount, _memo, false, false)
+        );
     }
 
     function getRequestsLength() public view returns (uint256) {
@@ -30,10 +44,34 @@ contract Prunto {
         returns (
             address,
             uint256,
-            string memory
+            string memory,
+            bool,
+            bool
         )
     {
-        Request memory req = requests[msg.sender][_id];
-        return (req.requester, req.amount, req.memo);
+        Request storage req = requests[msg.sender][_id];
+        return (req.requester, req.amount, req.memo, req.accepted, req.denied);
     }
+
+    function acceptRequest(uint256 _id) public {
+        Request storage req = requests[msg.sender][_id];
+        require(!req.accepted, "Request is already accepted.");
+        require(!req.denied, "Request is already denied.");
+        // todo actually pay requestor
+        loans[req.requester] = Loan(payable(msg.sender), req.amount);
+        req.accepted = true;
+    }
+
+    function denyRequest(uint256 _id) public {
+        Request storage req = requests[msg.sender][_id];
+        require(!req.accepted, "Request is already accepted.");
+        require(!req.denied, "Request is already denied.");
+        req.denied = true;
+    }
+
+    function getLoan() public view returns (Loan memory) {
+        return loans[msg.sender];
+    }
+
+    //todo makepayment
 }
