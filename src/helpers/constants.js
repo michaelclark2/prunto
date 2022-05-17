ebd2a0e022a6e0d0b7a27ea049c904ff23ad3704
Michael Clark <michael.clark1992@gmail.com>
2022-05-17 18:04:37 -0500

    updated abi.json

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,4 @@
 export const ERC20_DECIMALS = 18;
 
-export const PruntoContractAddress = "0x40A80A3f15D4B303F34935e92E2B4f2f3A0e2056";
+export const PruntoContractAddress = "0x305D7b403328b6CD8b204223BcFed552dA76e20f";
 export const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
